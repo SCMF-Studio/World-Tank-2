49bb6db8a9d4900730f024109b90da9aee3948e0
Scorp3y <artem.turovetc@gmail.com>
2024-08-03 11:03:12 +0300

    0.5.0V Random spawn Player

---
+++ Assets/RandomSpawn.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RandomSpawn : MonoBehaviour
+{
+    public GameObject tank;
+    public Transform SpawnOne, SpawnTwo, SpawnThree, SpawnFour, SpawnFive, SpawnSix, SpawnSeven;
+    private int pos;
+
+    
+    System.Random rnd = new System.Random();
+
+    
+    
+
+
+    void Start()
+    {
+        int value = rnd.Next(1, 6);
+        pos = value;
+        Debug.Log(value);
+        Possition();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+    private void Possition()
+    {
+        switch (pos)
+        {
+            case 1:
+                tank.transform.position = SpawnOne.transform.position;
+                break;
+            case 2:
+                tank.transform.position = SpawnTwo.transform.position;
+                break;
+            case 3:
+                tank.transform.position = SpawnThree.transform.position;
+                break;
+            case 4:
+                tank.transform.position = SpawnFour.transform.position;
+                break;
+            case 5:
+                tank.transform.position = SpawnFive.transform.position;
+                break;
+            case 6:
+                tank.transform.position = SpawnSix.transform.position;
+                break;
+            case 7:
+                tank.transform.position = SpawnSeven.transform.position;
+                break;
+
+
+        }
+    }
+}
